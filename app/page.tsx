8a31723fac169cfcc6f28891ace4caf96d263896
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-03-31 15:11:49 -0500

    Creacion del proyecto en next.js

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background text-foreground">
+      <h1 className="text-3xl font-semibold p-10">
+        Portafolio en construcción
+      </h1>
+    </main>
+  );
+}
