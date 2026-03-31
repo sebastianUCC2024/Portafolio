7bb3e46dfd360f33b54bcdd0a0f995efdd124bf1
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-03-31 15:23:12 -0500

    Correcion del proyecto

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background text-foreground">
-      <h1 className="text-3xl font-semibold p-10">
-        Portafolio en construcción
-      </h1>
-    </main>
-  );
-}
